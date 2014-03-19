70348966132a1ec353b9a5b6795ae3371b8be2c7
Guillermo Beltri <gobeltri@gmail.com>
2014-03-19 16:10:13 +0100

    test

--- web.js
+++ web.js
@@ -2,10 +2,13 @@ var express = require('express');
 var app = express();
 app.use(express.logger());
 
+
 var buffer = fs.readFileSync('index.html');
+var string = buffer.toString;
 
 app.get('/', function(request, response) {
-  response.send(buffer.toString);
+  response.send('Hello World 2!');
+  buffer.troString('utf-8');
 });
 
 var port = process.env.PORT || 5000;
