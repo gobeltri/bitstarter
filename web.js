e5600681099e901fdfc996642443030f60ae95e8
Guillermo Beltri <gobeltri@gmail.com>
2014-03-19 16:21:39 +0100

    Tested buffers

--- web.js
+++ web.js
@@ -2,13 +2,12 @@ var express = require('express');
 var app = express();
 app.use(express.logger());
 
-
+var fs = require('fs');
 var buffer = fs.readFileSync('index.html');
-var string = buffer.toString;
+var string = buffer.toString('utf-8');
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
-  buffer.troString('utf-8');
+  response.send(string);
 });
 
 var port = process.env.PORT || 5000;
